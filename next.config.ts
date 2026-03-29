188b0dcef3d472bd3782ec7c626a22fec4556104
Boldprism <thomas.kieu@boldprism.fr>
2026-03-29 17:17:47 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-headers: async () => [{
-  source: '/_next/static/:path*',
-  headers: [{ key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }],
-}]
-  import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
 };
 
 export default nextConfig;
+headers: async () => [{
+  source: '/_next/static/:path*',
+  headers: [{ key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }],
+}]
