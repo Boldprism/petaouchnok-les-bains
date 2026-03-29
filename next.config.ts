900e086ffdec8ddfd0e8fae6d0afd291d854aa40
Boldprism <thomas.kieu@boldprism.fr>
2026-03-29 17:04:18 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
-import type { NextConfig } from "next";
+headers: async () => [{
+  source: '/_next/static/:path*',
+  headers: [{ key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }],
+}]
+  import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
