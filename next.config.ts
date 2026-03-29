89d8cb09b07281a358ab818aa78513a9c2e34dee
KIEU Thomas <thomas@Mac.lan>
2026-03-29 17:28:29 +0200

    fix: next.config cache headers

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
+  headers: async () => [{
+    source: '/_next/static/:path*',
+    headers: [{ key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }],
+  }],
 };
 
 export default nextConfig;
-headers: async () => [{
-  source: '/_next/static/:path*',
-  headers: [{ key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }],
-}]
